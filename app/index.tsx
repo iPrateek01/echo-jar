2c8d7015ae28b3512ca372abf5d9e469f4a77b5e
Prateek Shende <prateek_655@live.com>
2025-06-05 20:36:43 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        height: "100%",
+        backgroundColor: "",
+      }}
+    >
+      <Text>Hello</Text>
+    </View >
+  );
+}
